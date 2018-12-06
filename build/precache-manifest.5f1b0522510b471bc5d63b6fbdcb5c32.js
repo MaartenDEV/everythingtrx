70be643e954b959d9c11f2e106939cfa71e4e780
Maarten Oudejans <maart@Maartens-MacBook-Pro.local>
2018-12-06 18:26:39 +0100

    ASD

---
+++ build/precache-manifest.5f1b0522510b471bc5d63b6fbdcb5c32.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a5fc9af51acf7b6c3662be952e502d1",
+    "url": "/static/media/boekflipped2.5a5fc9af.png"
+  },
+  {
+    "revision": "f80d0b2c27ea30fa0dd9",
+    "url": "/static/css/main.085549b5.chunk.css"
+  },
+  {
+    "revision": "8899f5a7f5e7ffea43b053eae56d1efc",
+    "url": "/static/media/trxicon.8899f5a7.ico"
+  },
+  {
+    "revision": "7e88e55ddf45588629b6",
+    "url": "/static/js/1.7e88e55d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9bb36a11d0a6d348612cf6172f6c770",
+    "url": "/static/media/boek2.a9bb36a1.png"
+  },
+  {
+    "revision": "5a321062b21d230885447020415df33f",
+    "url": "/static/media/chartexplode_resized.5a321062.png"
+  },
+  {
+    "revision": "7c799e7dd00869cd25e3cea69cfef6aa",
+    "url": "/static/media/fotobackground.7c799e7d.jpg"
+  },
+  {
+    "revision": "35b96a22a6ff6c3c1120bfc8459f2eef",
+    "url": "/static/media/innopaylogo.35b96a22.png"
+  },
+  {
+    "revision": "bb7dcfc5ddf2d1f0e43e50fddeb83fd4",
+    "url": "/static/media/surpisebox_resized.bb7dcfc5.png"
+  },
+  {
+    "revision": "18ca84166188d0ccdb8929ac4db2c5e5",
+    "url": "/static/media/boekflipped2.18ca8416.png"
+  },
+  {
+    "revision": "f80d0b2c27ea30fa0dd9",
+    "url": "/static/js/main.f80d0b2c.chunk.js"
+  },
+  {
+    "revision": "1e5e9c2d6c5dcee9ebb04c55034b0ae5",
+    "url": "/static/media/MaisonNeue-Bold.1e5e9c2d.woff"
+  },
+  {
+    "revision": "57a0905ea579114563a4a0b89b8ca9c5",
+    "url": "/static/media/MaisonNeue-Book.57a0905e.woff"
+  },
+  {
+    "revision": "b0c1c4acb0931a980afe0fc3dc07e2a4",
+    "url": "/static/media/ChronicleTextG2-Roman.b0c1c4ac.woff"
+  },
+  {
+    "revision": "4d1beb8437a806c0ad6f89e428e5d54d",
+    "url": "/static/media/ChronicleTextG2-Semi.4d1beb84.woff"
+  },
+  {
+    "revision": "4c13a4bde47f0b6a41cfca220ac2246e",
+    "url": "/static/media/MaisonNeue-Book.4c13a4bd.ttf"
+  },
+  {
+    "revision": "30ddd41540e51afcdbaa361b1bc5a52a",
+    "url": "/static/media/MaisonNeue-Bold.30ddd415.ttf"
+  },
+  {
+    "revision": "920424965a05505d4a239dfa2c241d0d",
+    "url": "/static/media/ChronicleTextG2-Semi.92042496.ttf"
+  },
+  {
+    "revision": "7e9890b207edef882f8e4d7b22e807e6",
+    "url": "/static/media/ChronicleTextG2-Roman.7e9890b2.ttf"
+  },
+  {
+    "revision": "383903bcd4d48a117af73cb5b620755f",
+    "url": "/static/media/fotobackground2.383903bc.jpg"
+  },
+  {
+    "revision": "7e88e55ddf45588629b6",
+    "url": "/static/css/1.23c97753.chunk.css"
+  },
+  {
+    "revision": "bcd4f8688f70add27e7fc0e41ee6eeca",
+    "url": "/index.html"
+  }
+];
